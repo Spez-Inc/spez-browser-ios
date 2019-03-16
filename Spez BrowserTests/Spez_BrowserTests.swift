221f9bf502ac05e8ab66cb0715ddb4208048205e
Sarp Ertoksoz <sarpertoksoz@gmail.com>
2019-03-16 08:16:39 +0300

    first commit

---
+++ Spez BrowserTests/Spez_BrowserTests.swift
@@ -0,0 +1,36 @@
+//
+//  Spez_BrowserTests.swift
+//  Spez BrowserTests
+//
+//  Created by Konuk Kullanıcı on 14.03.2019.
+//  Copyright © 2019 Spez Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Spez_Browser
+
+class Spez_BrowserTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
